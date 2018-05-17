322d534cdf7ab4b717d096fa6bc03ee78d93c612
DarthVader <fox.mulder.trustnoone@gmail.com>
2018-05-17 18:09:35 +0300

    Some code combing

--- loader.py
+++ loader.py
@@ -10,7 +10,7 @@ import random
 from datetime import datetime
 from time import sleep
 import pytz
-import os, json, csv
+import os, json, csv, pickle
 import ccxt.async as ccxt
 from ccxt.async import Exchange
 from utils.tail import tail
@@ -31,30 +31,50 @@ my_exchanges = [
 ]
 
 
-# фиатные валюты
+# fiat currencies
 fiat = ['USD','EUR','JPY','UAH','USDT','RUB','CAD','NZDT']
-# токены с низкими комиссиями
 low_fee_tokens = ['ADA','ZEC','ETC','EOS','DOGE','XDG','XRP','BTG','NEO','LTC','BCH']
-# токены с большим объёмом
 high_volume_tokens = ['BTC','ETH','BCH','LTC','ADA','XRP','XMR','ETC','NEO',
                       'EOS','ZEC','XLM','DASH','MIOTA','TRX','XEM','BTG',
                       'OMG','ICX','XVG','DOGE','SC','DCR']
+# custom tokens
 my_tokens = ['ADA','ZEC','ETC','EOS','DOGE','XDG','XRP','BTG','NEO','LTC','BCH',
              'BTC','XMR','ETH','XLM','DASH','TRX','XEM','OMG','ICX','XVG','DCR','MIOTA','SC',
              'USD','USDT'
              ]
 
 
-async def OrderBook(exchange, product):
+async def OrderBook(exchange, pair, last_fetch):
     #process_time = random.randint(1,5)
     while True:
-        orderbook = await exchange.fetch_order_book(product)
-        
-        #print("Fetch {} has completed after {} seconds".format(exchange_name, process_time))
-        print("{:%d.%m.%Y %H:%M:%S} Orderbook: {}, {} bids={}, asks={}".format(
-                datetime.now(), exchange.name, product, 
-                orderbook['bids'][0], orderbook['asks'][0]))
-        await asyncio.sleep(exchange.rateLimit/1000)
+        try:
+            ts = last_fetch[exchange.name.lower()][pair]['history']
+            if ts == None:
+                orderbooks = await exchange.fetch_order_book(pair, limit=100)
+            else:
+                orderbooks = await exchange.fetch_order_book(pair, since=ts)        
+
+            orderbook = await exchange.fetch_order_book(pair)
+            
+            #filename = "data/{}.{}.csv".format(exchange.name, pair.replace("/","-"))
+            filename = "{}/{}/orderbook_log.csv".format(os.getcwd(), data_folder)
+            with open(filename, "a") as file:
+                line = "{};{};{};{};{};{};{};{};{}".format(datetime.now(),
+                        orderbook['timestamp'], exchange.name, pair, 
+                        orderbook['price'], orderbook['amount'], orderbook['type'], orderbook['side'], ts)
+                file.write(line + "\n")
+
+            #print("Fetch {} has completed after {} seconds".format(exchange_name, process_time))
+            print("{:%d.%m.%Y %H:%M:%S} Orderbook: {}, {} bids={}, asks={}".format(
+                    datetime.now(), exchange.name, pair, 
+                    orderbook['bids'][0], orderbook['asks'][0]))
+
+        except IOError:
+            print("File access error")
+        except:
+            pass
+        finally:
+            await asyncio.sleep(exchange.rateLimit/1000)
 
 
 async def History(exchange, pair, last_fetch):
@@ -69,7 +89,7 @@ async def History(exchange, pair, last_fetch):
             history = histories[-1]
             last_fetch[exchange.name.lower()][pair]['history'] = history['timestamp'] + 1
                         
-            filename = "{}\\history\\{}.{}.csv".format(data_folder, exchange.name, pair.replace("/","-"))
+            filename = "{}/{}/history/{}.{}.csv".format(os.getcwd(), data_folder, exchange.name, pair.replace("/","-"))
             with open(filename, "a") as file:
                 for x in histories:
                     txt = "{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}{0}{7}{0}{8}{0}{9}".format(
@@ -79,12 +99,12 @@ async def History(exchange, pair, last_fetch):
                     file.write(txt + "\n")
 
             #filename = "data/{}.{}.csv".format(exchange.name, pair.replace("/","-"))
-            # filename = "data/history.csv"
-            # with open(filename, "a") as file:
-            #     line = "{};{};{};{};{};{};{};{};{}".format(datetime.now(),
-            #             history['timestamp'], exchange.name, pair, 
-            #             history['price'], history['amount'], history['type'], history['side'], ts)
-            #     file.write(line + "\n")
+            filename = "{}/{}/history_log.csv".format(os.getcwd(), data_folder)
+            with open(filename, "a") as file:
+                line = "{};{};{};{};{};{};{};{};{}".format(datetime.now(),
+                        history['timestamp'], exchange.name, pair, 
+                        history['price'], history['amount'], history['type'], history['side'], ts)
+                file.write(line + "\n")
             
             print("{:%d.%m.%Y %H:%M:%S} History  : {}, {} {} {} price={} amount={}".format(
                     datetime.now(), exchange.name, history['symbol'], history['side'], 
@@ -94,7 +114,8 @@ async def History(exchange, pair, last_fetch):
             print("File access error")
         except:
             pass
-        await asyncio.sleep(exchange.rateLimit/1000)
+        finally:
+            await asyncio.sleep(exchange.rateLimit/1000)
 
 
 async def LoadMarkets(exchange):
@@ -103,21 +124,35 @@ async def LoadMarkets(exchange):
 
 
 async def main():
+
     # create exchanges objects
+    print("Initialize exchange objects...", end="", flush=True)
     exchanges = {}
     for id in my_exchanges:
         exchange = getattr(ccxt, id)
         exchange.enableRateLimit = True, # this option enables the built-in rate limiter <<=============
         exchanges[id] = exchange()
+    print("Done.")
+
+    print("Checking folder structure and loading last access time for each commodity...", end="", flush=True)
+    folders = ['history', 'orderbook']
+    for folder in folders:
+        os.makedirs(os.path.join(os.getcwd()) + "/" + data_folder + "/" + folder, exist_ok=True)
+    print("Done.")
 
     print("Loading exchanges...")
+    
     tasks = []
     for ex in exchanges.items():
         tasks.append(asyncio.ensure_future(LoadMarkets(ex[1])))
     await asyncio.gather(*tasks)
-    print("Done.\n")
 
-    print("Detecting active symbols and generating pairs...")
+    # saving to exchange cache
+    #with open("{}/{}/exchanges.dat".format(os.getcwd(), data_folder), "wb") as handle:
+    #    pickle.dump(exchanges, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    #print("Done.")
+
+    print("Detecting active symbols and generating pairs...", end="", flush=True)
     ex_pairs = {}
     for ex in my_exchanges:
         my_pairs = [sym.split('/') for sym in exchanges[ex].symbols 
@@ -128,13 +163,9 @@ async def main():
     #symbols = list(set(symbols))
     #allowed_pairs = [sym.split('/') for sym in symbols if sym.split('/')[0] in my_tokens and sym.split('/')[1] in my_tokens]
     #my_pairs = [x[0]+'/'+x[1] for x in allowed_pairs]
-    print("Done.\n")
+    print("Done.")
 
-    print("Checking folder structure and loading last access time for each commodity...")
-    folders = ['history', 'orderbook']
-    for folder in folders:
-        os.makedirs(os.path.join(os.getcwd()) + "/" + data_folder + "/" + folder, exist_ok=True)
-
+    print("Fetching last access time for each pair...", end="", flush=True)
     last_fetch = {} # init dict with last fetches
     try:
         for ex in my_exchanges:
@@ -155,7 +186,8 @@ async def main():
                 
     except Exception:
         pass
-    print("Done.\n")
+    print("Done.")
+
 
     try:
         tasks = []
@@ -163,8 +195,7 @@ async def main():
         for ex in exchanges.items(): # running concurrent tasks to fetch data
             ex_obj, exchange = ex[1], ex[0]
             for pair in ex_pairs[exchange]:
-                #tasks.append(asyncio.ensure_future(OrderBook(ex_obj, pairs)))
-                #tasks.append(asyncio.ensure_future(History(ex_obj, pairs)))
+                #tasks.append(asyncio.ensure_future(OrderBook(ex_obj, pair, last_fetch)))
                 tasks.append(asyncio.ensure_future(History(ex_obj, pair, last_fetch)))
     except Exception:
         pass
