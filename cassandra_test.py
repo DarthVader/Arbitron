5511466ae821aaffa2458402d1a205c96b0f4088
DarthVader <fox.mulder.trustnoone@gmail.com>
2018-06-06 17:02:23 +0300

    Python cassandra API test

--- cassandra_test.py
+++ cassandra_test.py
@@ -7,16 +7,17 @@ from cassandra.query import dict_factory
 def pandas_factory(colnames, rows):
     return pd.DataFrame(rows, columns=colnames)
 
-#auth_provider = PlainTextAuthenticator(username='', password='')
-cluster = Cluster(contact_points=['10.7.0.10'], port=9042) #, auth_provider=auth_provider)
+#auth_provider = PlainTextAuthenticator(username='cassandra', password='cassandra')
+cluster = Cluster(contact_points=['10.7.0.21'], port=9042) #, auth_provider=auth_provider)
+#cluster = Cluster(contact_points=['10.7.0.21'], port=9042)
 
 session = cluster.connect(keyspace='history')
 session.row_factory = pandas_factory
-session.default_fetch_size = None
+session.default_fetch_size = 10
 
-cql = "SELECT id, name FROM test"
+cql = "SELECT transaction_date, exchange, symbol, price, amount, side FROM history.binance LIMIT 20"
 
-res = session.execute(cql, timeout=None)
+res = session.execute(cql, timeout=5)
 df = res._current_rows
 
 print(df)
