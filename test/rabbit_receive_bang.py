fa16343e1b0f9fe65d5d1a2f3633cd2e95958e11
DarthVader <fox.mulder.trustnoone@gmail.com>
2018-07-03 18:53:38 +0300

    RabbitMQ pacemaker test

---
+++ test/rabbit_receive_bang.py
@@ -0,0 +1,27 @@
+# bang message client
+
+import pika
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received '{}'".format(body))
+
+
+if __name__ == '__main__':
+    try:
+        cred = pika.credentials.PlainCredentials(username="mike", password="cawa")
+        connection = pika.BlockingConnection(pika.ConnectionParameters('10.7.0.11', credentials=cred))
+        channel = connection.channel()
+
+        channel.queue_declare(queue="bang")
+        channel.basic_consume(callback, queue="bang", no_ack=True)
+
+        print(" [*] Waiting for messages. To exit press Ctrl+C")
+
+        channel.start_consuming()
+
+    except KeyboardInterrupt:
+        print("\nLeaving by CTRL-C")
+
+    except Exception as e:
+        print(e)
---
+++ test/rabbit_send_fizzbuzz.py
@@ -0,0 +1,37 @@
+# messaging server
+
+import pika
+import sys
+from time import sleep
+
+if __name__ == '__main__':
+    cred = pika.credentials.PlainCredentials(username="mike", password="cawa")
+    connection = pika.BlockingConnection(pika.ConnectionParameters('10.7.0.11', credentials=cred))
+    
+    channel = connection.channel()
+    channel.queue_declare(queue="test")
+
+    # round-robin message send
+    msg = "fizz"
+    for i in range(1,1000):
+        try:
+            channel.basic_publish(exchange="", routing_key=msg, body="{} - {}".format(msg, i))
+            print(" [x] Test message {} has been sent".format(i))
+            # iterate through fizz-buzz-bang
+            if msg=="fizz":
+                msg = "buzz"
+            elif msg=="buzz":
+                msg = "bang"
+            else:
+                msg = "fizz"
+            sleep(0.33)
+        except KeyboardInterrupt:
+            print("\nLeaving by CTRL-C")
+            connection.close()
+            sys.exit()
+
+        except Exception as e:
+            connection.close()
+            print(e)
+
+    connection.close()
